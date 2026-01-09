8875ca031e1fb562c9744a8122fd15ae2ddf0e21
portfiend <109661617+portfiend@users.noreply.github.com>
2026-01-09 15:26:10 -0600

    make this shit predicted

--- Content.Client/_DEN/Species/EntitySystems/PhysiologyDescriptionSystem.cs
+++ Content.Client/_DEN/Species/EntitySystems/PhysiologyDescriptionSystem.cs
@@ -1,7 +1,7 @@
 using Content.Shared._DEN.Species.EntitySystems;
 
 #pragma warning disable IDE1006 // Naming Styles
-namespace Content.Server._DEN.Species.EntitySystems;
+namespace Content.Client._DEN.Species.EntitySystems;
 #pragma warning restore IDE1006 // Naming Styles
 
 public sealed partial class PhysiologyDescriptionSystem : SharedPhysiologyDescriptionSystem
